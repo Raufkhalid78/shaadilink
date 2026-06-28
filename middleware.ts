48a9d8651dac602d3c0d58d959876271d37e58c4
Rauf Khalid <rauf.khaled78@gmail.com>
2026-06-28 20:48:52 +0300

    fix: update CSP to allow Google Maps and Unsplash images to render in viewer

--- middleware.ts
+++ middleware.ts
@@ -8,12 +8,12 @@ export async function middleware(request: NextRequest) {
     default-src 'self';
     script-src 'self' 'unsafe-eval' 'unsafe-inline';
     style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
-    img-src 'self' blob: data: https://nldoyrprekstnifrlblo.supabase.co;
+    img-src 'self' blob: data: https://nldoyrprekstnifrlblo.supabase.co https://images.unsplash.com https://lh3.googleusercontent.com;
     font-src 'self' data: https://fonts.gstatic.com;
     object-src 'none';
     base-uri 'self';
     form-action 'self';
-    frame-src 'self' https://www.youtube.com https://sandbox.api.getsafepay.com;
+    frame-src 'self' https://www.youtube.com https://sandbox.api.getsafepay.com https://maps.google.com https://www.google.com;
     frame-ancestors 'none';
     connect-src 'self' https://nldoyrprekstnifrlblo.supabase.co;
     upgrade-insecure-requests;
